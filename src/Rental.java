fd0bedc8bfb3d5363c5aaffa1a67169f33526da6
VKalushev <vasko.kalushev@gmail.com>
2024-06-13 16:24:57 +0300

    Functionality and Business Rules fully implemented

--- src/Rental.java
+++ src/Rental.java
@@ -1,6 +1,75 @@
 package src;
 
+import src.vehicles.Car;
+import src.vehicles.Vehicle;
+
+import java.time.LocalDate;
+
 public class Rental {
-    public Rental() {
+    private Vehicle vehicle;
+    private int rentalPeriod;
+    private double dailyRentalCost;
+    private Customer customer;
+    private LocalDate reservationStartDate;
+    private LocalDate reservationEndDate;
+
+    public Rental(Vehicle vehicle, int rentalPeriod, Customer customer, LocalDate reservationStartDate) {
+        this.vehicle = vehicle;
+        this.rentalPeriod = rentalPeriod;
+        this.customer = customer;
+        this.reservationStartDate = reservationStartDate;
+        this.reservationEndDate = reservationStartDate.plusDays(rentalPeriod);
+
+        if(rentalPeriod > 7){
+            switch (vehicle.getType()) {
+                case "Car" -> this.dailyRentalCost = 15;
+                case "Motorcycle" -> this.dailyRentalCost = 10;
+                case "CargoVan" -> this.dailyRentalCost = 40;
+            }
+        } else {
+            switch (vehicle.getType()) {
+                case "Car" -> this.dailyRentalCost = 20;
+                case "Motorcycle" -> this.dailyRentalCost = 15;
+                case "CargoVan" -> this.dailyRentalCost = 50;
+            }
+        }
+
+
+    }
+
+    public String getCustomName() {
+        return this.customer.getName();
+    }
+
+    public int getRentalPeriod() {
+        return rentalPeriod;
+    }
+
+    public double getDailyRentalCost() {
+        return dailyRentalCost;
+    }
+
+    public LocalDate getReservationStartDate() {
+        return reservationStartDate;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public LocalDate getReservationEndDate() {
+        return reservationEndDate;
+    }
+
+    public String getRentedVehicleName() {
+        return this.vehicle.getBrand() + " " + this.vehicle.getModel();
+    }
+
+    public int getCustomerAge() {
+        return this.customer.getAge();
+    }
+
+    public int getCustomerDrivingExperience() {
+        return this.customer.getDrivingExperience();
     }
 }
--- src/VehicleRentalSystem.java
+++ src/VehicleRentalSystem.java
@@ -1,4 +1,34 @@
 package src;
 
+import src.vehicles.Car;
+import src.vehicles.CargoVan;
+import src.vehicles.Motorcycle;
+import src.vehicles.Vehicle;
+
+import java.time.LocalDate;
+
 public class VehicleRentalSystem {
+    public static void main(String[] args) {
+
+//        Vehicle vehicle = new Car("Mitsubishi","Mirage",15000,3);
+//        Vehicle vehicle = new Motorcycle("Triumph","Tiger Sport 660",10000);
+        Vehicle vehicle = new CargoVan("Citroen","Jumper",20000);
+
+        Customer customer = new Customer("John Doe");
+
+        if(vehicle.getType().equals("Motorcycle")){
+            customer.setAge(20);
+        } else if(vehicle.getType().equals("CargoVan")){
+            customer.setDrivingExperience(8);
+        }
+
+        LocalDate reservationStartData = LocalDate.of(2024, 6, 3);
+
+        Rental rental = new Rental(vehicle,4,customer,reservationStartData);
+
+        LocalDate actualReturnDate = LocalDate.of(2024,6,13);
+        Invoice invoice = new Invoice(rental,actualReturnDate);
+        invoice.generateInvoice();
+
+    }
 }
